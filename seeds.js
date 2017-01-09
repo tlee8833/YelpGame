70c6df1654ee8f08eedcb0da8e33c7c40a7cb148
Tony <tlee8833@gmail.com>
2017-01-09 22:21:51 +0000

    change seeds.js

--- seeds.js
+++ seeds.js
@@ -6,27 +6,27 @@ var data = [
     {
         name: "Hearthstone", 
         image: "http://blogs-images.forbes.com/insertcoin/files/2014/12/hearthstone1.jpg",
-        description: "Hearthstone is a free-to-play online collectible card video game developed and published by Blizzard Entertainment. It is the known for its casual play style and RNG."
+        description: "Hearthstone is an online card game developed by Blizzard Entertainment. It is the known for its casual play style."
     },
     {
         name: "Shadowverse", 
         image: "http://img.youtube.com/vi/hIiMFeNx3_I/maxresdefault.jpg",
-        description: "Shadowverse is a free-to-play collectible card video game developed and published by Cygames. It is similar to Hearthstone with an anime style. The developers try to minimize the impact of randomness and create \"Evolve mechanism\" to make this game more strategic."
+        description: "Shadowverse is an online card game developed by Cygames. The developers try to minimize the impact of randomness and create \"Evolve mechanism\" to make this game more strategic."
     },
     {
         name: "Faeria", 
         image: "https://www.faeria.com/bundles/app/images/socials/aurora_logo_1200x630.jpg",
-        description: "Board card game"
+        description: "Faeria is an online card game developed by Abrakam. It has a hex-based playfield, so positioning and movement matters a lot. Your mana can be saved for upcoming turns. Aggro/midrange/control are all viable."
     },
     {
         name: "Duelyst", 
         image: "https://i.ytimg.com/vi/rHUdNbKvu5c/maxresdefault.jpg",
-        description: "Duelyst is a free-to-play collectible card game on a 2D battlefield."
+        description: "Duelyst is an online card game developed by Counterplay Games. It has a 5*9 battlefield, so positioning and movement matters a lot. It's a strongly tempo-based game, so each game is relatively short."
     },
     {
         name: "Eternal", 
         image: "http://cdn.akamai.steamstatic.com/steam/apps/531640/header.jpg?t=1481652157",
-        description: "Eternal combines the infinite possibilities of a deep strategy card game with the pace and polish of a modern video game."
+        description: "Eternal is an online card game developed by Dire Wolf Digital. In this game, your opponent chooses his/her way to block your attacking unit, and you can react with fast spells in your opponent's turn, which makes this game very strategic."
     }
 ]
 
